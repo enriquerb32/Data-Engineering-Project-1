880d90826d8fe9377ba8d4ecf0ae122cd172aaa3
Enrique Real Bru <102382251+enriquerb32@users.noreply.github.com>
2023-11-19 01:02:09 +0100

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -2,7 +2,7 @@
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "eu-north-1"
+  default     = "us-east-1"
 }
 
 ## Key to allow connection to our EC2 instance
